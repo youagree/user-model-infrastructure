af9b07b7d35302e92e3e878692a00f8746a39b6c
Alexey Vasin <empty>
2021-06-07 23:11:37 +0300

    init commit
    author: uber-dev

---
+++ user-model-infrastructure-impl/src/main/java/ru/unit_techno/user/model/impl/config/UserModelConfig.java
@@ -0,0 +1,10 @@
+
+package ru.unit_techno.user.model.impl.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("ru.unit_techno.user.model.impl")
+public class UserModelConfig {
+}
---
+++ user-model-infrastructure-impl/src/main/java/ru/unit_techno/user/model/impl/entity/UserEntity.java
@@ -0,0 +1,5 @@
+
+package ru.unit_techno.user.model.impl.entity;
+
+public class UserEntity {
+}
