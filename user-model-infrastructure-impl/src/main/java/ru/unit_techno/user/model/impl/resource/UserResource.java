5496a70f20dd1396a73d33214661c979daec00ef
Alexey Vasin <empty>
2021-06-17 20:47:44 +0300

    add user activation resource
    Issue: FR-RLMDL-AUTH-001

--- user-model-infrastructure-impl/src/main/java/ru/unit_techno/user/model/impl/resource/UserResource.java
+++ user-model-infrastructure-impl/src/main/java/ru/unit_techno/user/model/impl/resource/UserResource.java
@@ -3,7 +3,13 @@ package ru.unit_techno.user.model.impl.resource;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import ru.unit_techno.user.model.impl.dto.ActivateDto;
 import ru.unit_techno.user.model.impl.dto.DeleteUserDto;
 import ru.unit_techno.user.model.impl.dto.UserDto;
 import ru.unit_techno.user.model.impl.service.UserService;
@@ -19,6 +25,11 @@ public class UserResource {
     @PostMapping("/create-user")
     public UserDto createUser(@Valid @RequestBody UserDto userDto) {
         return userService.addUser(userDto);
+    }
+
+    @PostMapping("/activate-user")
+    public void activateUser(@Valid @RequestBody ActivateDto activateDto) {
+        userService.activateUser(activateDto);
     }
 
     @DeleteMapping("/delete-user")
